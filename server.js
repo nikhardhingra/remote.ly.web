3a5894849e793eeb938cf0ead3cb971ab942bb46
Nikhar Dhingra <dhingranikhar@gmail.com>
2021-03-23 21:28:56 +0530

    add stuff

--- server.js
+++ server.js
@@ -34,9 +34,8 @@ mongoose
   })
   .then(() => {
     console.log(`MongoDB Connected...`);
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch((err) => console.log(err));
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => `Server running on port ${PORT}`);
