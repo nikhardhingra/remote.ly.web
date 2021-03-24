a1992a5b9c1425f28c98f1b8ca2d0ce7eb5b77d7
ndhingra96 <ndhingra@edu.uwaterloo.ca>
2021-03-24 17:41:50 -0400

    Update Change Avatar

--- client/src/components/Profile/ChangeAvatar.js
+++ client/src/components/Profile/ChangeAvatar.js
@@ -150,7 +150,7 @@ class ChangeAvatar extends Component {
                 className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:w-auto sm:text-sm"
                 onClick={this.handleSubmit}
               >
-                Update Contact Info
+                Update Avatar
               </button>
               <button
                 type="button"
