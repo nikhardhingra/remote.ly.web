45eebb63be555c7d29efba271f0011ff82b77218
ndhingra96 <33238891+nikhardhingra@users.noreply.github.com>
2021-03-28 19:17:21 -0400

    styling change

--- client/src/components/Connect/Connect.js
+++ client/src/components/Connect/Connect.js
@@ -7,18 +7,20 @@ export default function Connect() {
   return (
     <>
       <Navbar isAuthenticated={true} />
-      <h3 className="text-blue-600 text-3xl">Connect</h3>
-       <h3 className="text-3xl text-center lg:text-left text-blue-700"> Connect </h3>
-      	<p className="my-6 col-span-4 bg-white border-purple-500 border rounded-lg p-6">
-            Connect with hapticians by searching for users by:
+    	 <div className="bg-gray-100 p-6 relative">
+        {/* <RoadMapNavbar /> */}
+        <h3 className="text-blue-600 text-2xl">
+          Connect </h3>
+        	<p className="mt-2">
+         	Connect with hapticians by searching for users by:
             <ol>
 			<li> 1. User name </li>
 			<li> 2. Skill </li>
 			<li> 3. Experience level (i.e. novice or expert) </li>
 			<li> 4. Project category </li>
              </ol>
-          </p>
-     
+       	 	</p>
+      		</div> 
       <div className="bg-gray-100 p-16 w-full">
         <Search />
       </div>
