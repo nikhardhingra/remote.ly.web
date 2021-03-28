c53bd0091c90777d0facd406650859603d67d0fc
ndhingra96 <33238891+nikhardhingra@users.noreply.github.com>
2021-03-28 19:30:47 -0400

    changed text and formatting

--- client/src/components/Connect/Connect.js
+++ client/src/components/Connect/Connect.js
@@ -9,18 +9,19 @@ export default function Connect() {
       <Navbar isAuthenticated={true} />
     	 <div className="bg-gray-100 p-6 relative">
         {/* <RoadMapNavbar /> */}
-        <h3 className="text-blue-600 text-2xl">
-          Connect </h3>
+        	<h3 className="text-blue-600 text-2xl">
+          	Connect </h3>
         	<p className="mt-2">
          	Connect with hapticians by searching for users by:
             <ol>
-			<li> 1. User name </li>
-			<li> 2. Skill </li>
-			<li> 3. Experience level (i.e. novice or expert) </li>
-			<li> 4. Project category </li>
+			<li> 1. Username</li>
+			<li> 2. Skill</li>
+			<li> 3. Experience level (i.e. novice or expert)</li>
+			<li> 4. Project category</li>
              </ol>
        	 	</p>
-      		</div> 
+      	</div> 
+      		<br />
       <div className="bg-gray-100 p-16 w-full">
         <Search />
       </div>
