2281b9c8db49e2263cd57ffb49a93cf0da26c327
ndhingra96 <33238891+nikhardhingra@users.noreply.github.com>
2021-03-28 19:03:39 -0400

    updated connect.js

--- client/src/components/Connect/Connect.js
+++ client/src/components/Connect/Connect.js
@@ -7,6 +7,20 @@ export default function Connect() {
   return (
     <>
       <Navbar isAuthenticated={true} />
+      <h3 className="text-blue-600 text-3xl">Connect</h3>
+
+      <div className="flex flex-col-reverse justify-center lg:flex-row items-center">
+            <p className="mt-2 text-2xl">
+            <ol>	Connect with hapticians by searching for users by:
+			<li> User name </li>
+			<li> Skill </li>
+			<li> Experience level (i.e. novice or expert) </li>
+			<li> Project category </li>
+             <br />
+             </ol>
+            </p>
+      </div>
+
       <div className="bg-gray-100 p-16 w-full">
         <Search />
       </div>
@@ -14,3 +28,6 @@ export default function Connect() {
     </>
   );
 }
+
+
+ 
