e10e898629f014be56b66c6ae028cdf8f5ec56e2
ndhingra96 <33238891+nikhardhingra@users.noreply.github.com>
2021-03-28 19:11:30 -0400

    updated content

--- client/src/components/Connect/Connect.js
+++ client/src/components/Connect/Connect.js
@@ -8,19 +8,17 @@ export default function Connect() {
     <>
       <Navbar isAuthenticated={true} />
       <h3 className="text-blue-600 text-3xl">Connect</h3>
-
-      <div className="flex flex-col-reverse justify-center lg:flex-row items-center">
-            <p className="mt-2 text-2xl">
-            <ol>	Connect with hapticians by searching for users by:
-			<li> User name </li>
-			<li> Skill </li>
-			<li> Experience level (i.e. novice or expert) </li>
-			<li> Project category </li>
-             <br />
+       <h3 className="text-3xl text-center lg:text-left text-blue-700"> Connect </h3>
+      	<p className="my-6 col-span-4 bg-white border-purple-500 border rounded-lg p-6">
+            Connect with hapticians by searching for users by:
+            <ol>
+			<li> 1. User name </li>
+			<li> 2. Skill </li>
+			<li> 3. Experience level (i.e. novice or expert) </li>
+			<li> 4. Project category </li>
              </ol>
-            </p>
-      </div>
-
+          </p>
+     
       <div className="bg-gray-100 p-16 w-full">
         <Search />
       </div>
@@ -28,6 +26,3 @@ export default function Connect() {
     </>
   );
 }
-
-
- 
